dfce7e6ccdd929a0385d4c34f343c785b82a11e0
vtdeleond <vtdeleond@gmail.com>
2016-10-30 01:51:08 -0400

    Bonus changes

--- Assignment/001/app.js
+++ Assignment/001/app.js
@@ -8,14 +8,17 @@
 
   function MyController($scope) {
     // $scope.items = "";
+    $scope.valid = null;
     $scope.check = function() {
       var msg = $scope.items || "";
-      if ( msg.length == 0) {
+      var count = msg.split(',').filter( a => a.trim()).length;
+      if ( count == 0) {
         $scope.msg = "Please enter data first";
+        $scope.valid = false;
         return;
       }
       // var count = msg.split(',').filter(Boolean).length;
-      var count = msg.split(',').filter( a => a.trim()).length;
+      $scope.valid = true;
       $scope.msg = (count > 3) ? "Too much!" : "Enjoy!" ;
     };
   }
